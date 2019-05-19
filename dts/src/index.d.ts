42f8f9ee2bb4c7d05f829345a7ee13f8b23a3c12
pavan palakshamurthy <ppavanp@gmail.com>
2019-05-19 19:46:44 -0400

    rectified the main distribution file

--- dts/src/index.d.ts
+++ dts/src/index.d.ts
@@ -3,4 +3,5 @@ import IService from './service/interface/IService';
 import Service from './service/implementation/Service';
 import Request from './models/Request';
 import Response from './models/Response';
+
 export { BootStrap, IService, Service, Request, Response };
