60efee26d7e2ca0ed479c5cd78100e63a9e0976f
pavan palakshamurthy <ppavanp@gmail.com>
2019-05-19 23:37:31 -0400

    adds source map

--- dts/src/index.d.ts
+++ dts/src/index.d.ts
@@ -3,5 +3,4 @@ import IService from './service/interface/IService';
 import Service from './service/implementation/Service';
 import Request from './models/Request';
 import Response from './models/Response';
-
 export { BootStrap, IService, Service, Request, Response };
